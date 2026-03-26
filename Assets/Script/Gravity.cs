6c6d2b3898e3dccb71c925dac7e15b135b72d02c
BOATNAKUBz <Natthaphon.sueb@bumail.net>
2026-03-26 10:34:11 +0700

    Addscript

--- Assets/Script/Gravity.cs
+++ Assets/Script/Gravity.cs
@@ -1,16 +1,44 @@
 using UnityEngine;
-
-public class Gavity : MonoBehaviour
+using System.Collections.Generic;
+using UnityEngine.WSA;
+public class Gravity : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public static List<Gravity> otherObj;
+    private Rigidbody _rb;
+    const float G = 0.00677f;
 
-    // Update is called once per frame
-    void Update()
+
+    private void Awake()
     {
-        
+        _rb = GetComponent<Rigidbody>();
+        if (otherObj == null)
+        {
+            otherObj = new List<Gravity>();
+        }
+        otherObj.Add(this);
     }
+    private void FixedUpdate()
+    {
+        foreach (Gravity obj in otherObj)
+        {
+            if (obj != this)
+            {
+                Attract(obj);
+            }
+            
+        }
+    }
+    void Attract(Gravity other)
+    {
+        Rigidbody otherRb = other._rb;
+        Vector3 direction = _rb.position - otherRb.position;
+             float distance = direction.magnitude;
+        if (distance == 0) return; //ป้องกันไม่ให้มีแรงดึงดูด
+
+        // F = G(m1 * m2)/r^2
+        float forceMagnitude = G * (_rb.mass * otherRb.mass) / Mathf.Pow(distance , 2);
+        Vector3 gravityForce = forceMagnitude * direction.normalized;
+        otherRb.AddForce(gravityForce); // ใส่แรงดึงดูดและทิศทางให้กับวัตถุ
+    }
+   
 }
